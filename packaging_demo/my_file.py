4972efe2513286451287976981911972fe2c9e3f
Sirapob Singcharoen <nicesirapob@gmail.com>
2024-06-10 12:37:19 +0700

    fix : add packaging_demo

--- packaging_demo/my_file.py
+++ packaging_demo/my_file.py
@@ -1,6 +1,7 @@
 import numpy as np
-from my_folder.my_nested_file import CONSTANT as CONSTANT2
-from my_other_file import CONSTANT
+
+from packaging_demo.my_folder.my_nested_file import CONSTANT as CONSTANT2
+from packaging_demo.my_other_file import CONSTANT
 
 MY_ARRAY = np.array([1, 2, 3])
 print(MY_ARRAY)
