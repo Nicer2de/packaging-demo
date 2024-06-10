680fbfe0242be208ed405c572788f6cda1310f0c
Sirapob Singcharoen <nicesirapob@gmail.com>
2024-06-10 12:29:34 +0700

    add pytest cov

--- run.sh
+++ run.sh
@@ -31,6 +31,65 @@ function lint:ci {
     SKIP=no-commit-to-branch pre-commit run --all-files
 }
 
+# execute tests that are not marked as `slow`
+function test:quick {
+    run-tests -m "not slow" ${@:-"$THIS_DIR/tests/"}
+}
+
+# (example) ./run.sh test tests/test_slow.py::test__slow_add
+function test {
+    # run only specified tests, if none specified, run all
+    PYTEST_EXIT_STATUS=0
+    python -m pytest -m "$THIS_DIR/tests/" \
+        --cov="$THIS_DIR/packaging_demo" \
+        --cov-report html \
+        --cov-report term \
+        --cov-report xml \
+        --junit-xml "$THIS_DIR/test-reports/report.xml" \
+        --cov-fail-under 50 || ((PYTEST_EXIT_STATUS+=$?))
+    mv coverage.xml "$THIS_DIR/test-reports/"
+    mv htmlcov "$THIS_DIR/test-reports/"
+    return $PYTEST_EXIT_STATUS
+}
+
+function run-tests {
+    PYTEST_EXIT_STATUS=0
+    python -m pytest ${@:-"$THIS_DIR/tests/"} \
+        --cov "${COVERAGE_DIR:-$THIS_DIR/src}" \
+        --cov-report html \
+        --cov-report term \
+        --cov-report xml \
+        --junit-xml "$THIS_DIR/test-reports/report.xml" \
+        --cov-fail-under 60 || ((PYTEST_EXIT_STATUS+=$?))
+    mv coverage.xml "$THIS_DIR/test-reports/" || true
+    mv htmlcov "$THIS_DIR/test-reports/" || true
+    mv .coverage "$THIS_DIR/test-reports/" || true
+    return $PYTEST_EXIT_STATUS
+}
+
+function test:ci {
+    INSTALLED_PKG_DIR="$(python -c 'import packaging_demo; print(packaging_demo.__path__[0])')"
+    # in CI, we must calculate the coverage for the installed package, not the src/ folder
+    COVERAGE_DIR="$INSTALLED_PKG_DIR" run-tests
+}
+
+function test:wheel-locally {
+    deactivate || true
+    rm -rf test-env || true
+    python -m venv test-env
+    source test-env/bin/activate
+    clean || true
+    pip install build
+    build
+    pip install ./dist/*.whl pytest pytest-cov
+    test:ci
+    deactivate || true
+}
+
+function serve-coverage-report {
+    python -m http.server --directory "$THIS_DIR/htmlcov/"
+}
+
 function build {
     python -m build --sdist --wheel "$THIS_DIR/"
 }
@@ -64,12 +123,13 @@ function publish:prod {
 }
 
 function clean {
-    rm -rf dist build
+    rm -rf dist build coverage.xml test-reports
     find . \
       -type d -name "*cache*" -or \
       -type d -name "*.dist-info" -or \
-      -type d -name "*.egg-info" \
-      -not -path "./venv/*" -exec rm -rf {} +
+      -type d -name "*.egg-info" -or \
+      -type d -name "*htmlcov" \
+      -not -path "*env/*" -exec rm -rf {} +
     echo "Clean up completed!"
 }
 
